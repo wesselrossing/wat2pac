05150a1e7d5da202a5f86ff601c53d446cb03b5e
Wessel Rossing <wessel.rossing@zemanta.com>
2015-10-16 16:42:59 +0200

    Adding items to the suitcase now works

--- java/com/zemanta/wat2pac/activities/Suitcase.java
+++ java/com/zemanta/wat2pac/activities/Suitcase.java
@@ -1,5 +1,6 @@
 package com.zemanta.wat2pac.activities;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -10,7 +11,6 @@ import com.zemanta.wat2pac.airtable.OnAirtableResponseListener;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -19,11 +19,13 @@ import android.widget.Toast;
 
 public class Suitcase extends Activity implements OnAirtableResponseListener
 {
-    private static final int PICK_ITEM_REQUEST = 1234;
-
-    private String id;
+	private static final int PICK_ITEM_REQUEST = 1234;
+	
+	private String id;
 	private Button addItems;
 	private GridView gridView;
+	
+	private String itemToAdd;
 	
 	protected void onResume()
 	{
@@ -40,32 +42,51 @@ public class Suitcase extends Activity implements OnAirtableResponseListener
 		{
 			@Override
 			public void onClick(View view) {
-                startActivityForResult(new Intent(Suitcase.this, Selector.class), PICK_ITEM_REQUEST);
+				startActivityForResult(new Intent(Suitcase.this, Selector.class), PICK_ITEM_REQUEST);
 			}
 		});
 		
 		Airtable.getInstance().get("Pack%20list/" + id, this);
 	}
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch(requestCode) {
-            case PICK_ITEM_REQUEST:
-                if (resultCode == RESULT_OK) {
-                    Log.i("Wat2pac", data.getStringExtra("itemID"));
-                }
-                break;
-        }
-    }
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch(requestCode) {
+			case PICK_ITEM_REQUEST:
+				if(resultCode == RESULT_OK) {
+					
+					itemToAdd = data.getStringExtra("itemID");
+				}
+				break;
+		}
+	}
 	
 	@Override
 	public void onAirtableResponse(String response)
 	{
-		Toast.makeText(this, response, Toast.LENGTH_LONG).show();
-		
 		try
 		{
-			gridView.setAdapter(new ItemsAdapter(new JSONObject(response).getJSONObject("fields").getJSONArray("What?")));
+			if(itemToAdd != null)
+			{
+				JSONObject packlist = new JSONObject(response);
+				JSONObject fields = packlist.getJSONObject("fields");
+				fields.remove("ID");
+				
+				if(!fields.has("What?"))
+				{
+					fields.put("What?", new JSONArray());
+				}
+				
+				fields.getJSONArray("What?").put(itemToAdd);
+				
+				itemToAdd = null;
+				
+				Airtable.getInstance().patch("Pack%20list/" + packlist.getString("id"), fields, this);
+			}
+			else
+			{
+				gridView.setAdapter(new ItemsAdapter(new JSONObject(response).getJSONObject("fields").getJSONArray("What?")));
+			}
 		}
 		catch(JSONException e)
 		{
