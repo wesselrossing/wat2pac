d7d5969f3b8ea07bf0f2b09553a3032fd68267bf
Wessel Rossing <wessel.rossing@zemanta.com>
2015-10-16 12:44:27 +0200

    Form XML layout work in progress

--- java/com/zemanta/wat2pac/activities/Form.java
+++ java/com/zemanta/wat2pac/activities/Form.java
@@ -1,28 +1,33 @@
 package com.zemanta.wat2pac.activities;
 
+import com.zemanta.wat2pac.R;
 import com.zemanta.wat2pac.airtable.Airtable;
 import com.zemanta.wat2pac.airtable.OnAirtableResponseListener;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class Form extends Activity
 {
+	private Spinner where;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		
-		Airtable.getInstance().get("Items?limit=3&view=Main%20View", new OnAirtableResponseListener()
+		setContentView(R.layout.form_layout);
+		
+		where = (Spinner) findViewById(R.id.where);
+		
+		Airtable.getInstance().get("Locations?view=Main%20View", new OnAirtableResponseListener()
 		{
 			@Override
-			public void onAirtableResponse(final String response)
+			public void onAirtableResponse(String response)
 			{
-				TextView textView = new TextView(Form.this);
-				textView.setText(response);
-				setContentView(textView);
+				
 			}
 			
 			@Override
---
+++ java/com/zemanta/wat2pac/adapter/LocationsAdapter.java
@@ -0,0 +1,34 @@
+package com.zemanta.wat2pac.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class LocationsAdapter extends BaseAdapter
+{
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
