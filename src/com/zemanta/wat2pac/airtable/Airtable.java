0c5db2ebd2c330602990fdba0fb9ac396f669d5d
Wessel Rossing <wessel.rossing@zemanta.com>
2015-10-16 10:52:54 +0200

    Airtable API_KEY is now hidden

--- src/com/zemanta/wat2pac/airtable/Airtable.java
+++ src/com/zemanta/wat2pac/airtable/Airtable.java
@@ -2,5 +2,5 @@ package com.zemanta.wat2pac.airtable;
 
 public class Airtable
 {
-	private static final String API_KEY = "keyRT3cAbmW17l7kN";
+	private static final String API_KEY = AirtableApiKey.API_KEY;
 }
