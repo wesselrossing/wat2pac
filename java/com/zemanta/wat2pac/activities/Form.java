94969f00d76b946c5430daeca378e3eac240a324
Wessel Rossing <wessel.rossing@zemanta.com>
2015-10-16 13:40:55 +0200

    Proof of concept Form screen completed

--- java/com/zemanta/wat2pac/activities/Form.java
+++ java/com/zemanta/wat2pac/activities/Form.java
@@ -4,12 +4,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.zemanta.wat2pac.R;
+import com.zemanta.wat2pac.adapter.ListTypeAdapter;
 import com.zemanta.wat2pac.adapter.LocationsAdapter;
 import com.zemanta.wat2pac.airtable.Airtable;
 import com.zemanta.wat2pac.airtable.OnAirtableResponseListener;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -17,6 +22,8 @@ public class Form extends Activity
 {
 	private Spinner where;
 	private Spinner when;
+	private Spinner list;
+	private Button go;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -70,6 +77,19 @@ public class Form extends Activity
 			public void onError(String error)
 			{
 				Toast.makeText(Form.this, error, Toast.LENGTH_LONG).show();
+			}
+		});
+		
+		list = (Spinner) findViewById(R.id.list);
+		list.setAdapter(new ListTypeAdapter());
+		
+		go = (Button) findViewById(R.id.go);
+		go.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				startActivity(new Intent(Form.this, Suitcase.class));
 			}
 		});
 	}
---
+++ java/com/zemanta/wat2pac/adapter/ListTypeAdapter.java
@@ -0,0 +1,62 @@
+package com.zemanta.wat2pac.adapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.zemanta.wat2pac.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class ListTypeAdapter extends BaseAdapter
+{
+	private String[] listTypes = new String[] { "Suggested pack list", "My pack list" };
+	private LayoutInflater inflater;
+	
+	@Override
+	public long getItemId(int position)
+	{
+		return 0;
+	}
+	
+	@Override
+	public int getCount()
+	{
+		return listTypes.length;
+	}
+	
+	@Override
+	public String getItem(int position)
+	{
+		return listTypes[position];
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
+		TextView textView;
+		
+		if(convertView != null)
+		{
+			textView = (TextView) convertView;
+		}
+		else
+		{
+			if(inflater == null)
+			{
+				inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			}
+			
+			textView = (TextView) inflater.inflate(R.layout.spinner_item, null);
+		}
+		
+		textView.setText(getItem(position));
+		
+		return textView;
+	}
+}
