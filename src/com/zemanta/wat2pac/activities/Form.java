21db944ce5310adbb74cb86591c00a03b8b13ce3
Wessel Rossing <wessel.rossing@zemanta.com>
2015-10-16 10:45:55 +0200

    Added first activity

---
+++ src/com/zemanta/wat2pac/activities/Form.java
@@ -0,0 +1,13 @@
+package com.zemanta.wat2pac.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Form extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+}
